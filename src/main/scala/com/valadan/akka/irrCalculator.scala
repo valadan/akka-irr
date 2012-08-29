40a0057c6aba65ba2fca86fab4f4761ff1bbaf6a
narendra valada <narendra.valada@gmail.com>
2012-08-28 23:20:19 -0400

    refactor

--- src/main/scala/com/valadan/akka/irrCalculator.scala
+++ src/main/scala/com/valadan/akka/irrCalculator.scala
@@ -1,20 +1,34 @@
 package com.valadan.akka
 
+import java.util.concurrent.CountDownLatch
+import scala.collection.mutable.ListBuffer
 import akka.actor._
 import akka.routing.RoundRobinRouter
+import akka.routing.Broadcast
 
 object irrCalculator extends App {
 
-  calculateWithActors(nrOfWorkers = 15, maxIterationCount = 20, absoluteAccuracy = 1E-7)
-
-  //calculate(maxIterationCount = 20, absoluteAccuracy = 1E-7)
+  calculateWithActors(nrOfWorkers = 12, nrOfMessages=500000, maxIterationCount = 20, absoluteAccuracy = 1E-7)
 
   sealed trait IrrMessage
-  case class Calculate(guess: Double, values: Array[Double]) extends IrrMessage
-  case class Work(guess: Double, values: Array[Double], attempt: Int) extends IrrMessage
-  case class IterationResult(guess: Double, ratio: Double, values: Array[Double], attempt: Int) extends IrrMessage
-  case class Result(irr: Double, values: Array[Double], duration: Long)
+  case class Calculate(maxIterationCount: Int, absoluteAccuracy: Double, values: Array[Double]) extends IrrMessage
+  case class WorkResult(maxIterationCount: Int, absoluteAccuracy: Double, values: Array[Double]) extends IrrMessage
+  case class Result(irr: Double, values: Array[Double]) extends IrrMessage
+  case class ShutdownWorkers extends IrrMessage
+  case class ShutdownListener extends IrrMessage
 
+  def untilLessThan(maxIterationCount: Int, absoluteAccuracy: Double, values: Array[Double]): Double = {
+    var guess = 0.05
+    for (i <- 0 until maxIterationCount) {
+      var result = calculateIrrFor(guess, values)
+      if (Math.abs(result) <= absoluteAccuracy) {
+        return guess - result
+      }
+      guess = guess - result
+    }
+    Double.NaN
+  }
+  
   def calculateIrrFor(guess: Double, values: Array[Double]): Double = {
 
     // the value of the function (NPV) and its derivate can be calculated in the same loop
@@ -28,89 +42,89 @@ object irrCalculator extends App {
 
   }
 
-  class Worker extends Actor {
-
+  class Worker() extends Actor {
+    
     def receive = {
-      case Work(guess, values, attempt) =>
-        val ratio = calculateIrrFor(guess, values)
-        sender ! IterationResult(guess - ratio, ratio, values, attempt + 1) // perform the work
+      case WorkResult(maxIterationCount, absoluteAccuracy, values) =>      
+        sender ! Result(untilLessThan(maxIterationCount, absoluteAccuracy, values), values) // perform the work
+      case ShutdownWorkers =>
+      	sender ! ShutdownWorkers
     }
   }
 
   class Master(nrOfWorkers: Int,
+    nrOfMessages: Int,
     maxIterationCount: Int,
     absoluteAccuracy: Double,
-    listener: ActorRef) extends Actor {
-
-    val start: Long = System.currentTimeMillis
+    listener: ActorRef, 
+    latch: CountDownLatch) extends Actor {
 
     val workerRouter = context.actorOf(
       Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
+      
+    var duration = System.currentTimeMillis
+    var nrOfResults = 0;
 
     def receive = {
-      case Calculate(guess, values) =>
-        workerRouter ! Work(guess, values, 0)
-      case IterationResult(guess, ratio, values, attempt) =>
-        if (Math.abs(ratio) <= absoluteAccuracy)
-          listener ! Result(guess, values, System.currentTimeMillis - start)
-        else if (attempt > maxIterationCount)
-          listener ! Result(Double.NaN, values, System.currentTimeMillis - start)
-        else
-          workerRouter ! Work(guess, values, attempt)
+      case Calculate(maxIterationCount, absoluteAccuracy, values) =>
+        workerRouter ! WorkResult(maxIterationCount, absoluteAccuracy, values)
+      case Result(irr, values) =>
+        listener ! Result(irr, values)
+        nrOfResults=nrOfResults+1
+        if(nrOfResults==nrOfMessages) {
+	        workerRouter ! ShutdownWorkers
+        }
+      case ShutdownWorkers =>
+        workerRouter ! Broadcast(Actors.poisonPill)
+		workerRouter ! Actors.poisonPill	
+		listener ! ShutdownListener
+      case ShutdownListener =>
+        listener ! Actors.poisonPill
+        context.stop(self)     
+    }
+    
+    override def  postStop = {
+      duration = System.currentTimeMillis - duration
+      println("\n\tCalculation complete: \t\t%s"
+          .format(duration))
+      latch.countDown
+    }
+      
+  }
+
+  class Listener(buf: ListBuffer[Double]) extends Actor {
+    def receive = {
+      case Result(irr, values) =>
+        buf.append(irr)
+      case ShutdownListener =>
+        sender ! ShutdownListener
     }
   }
 
-  class Listener extends Actor {
-    def receive = {
-      case Result(irr, values, duration) =>
-        println("\n\tIRR approximation: \t\t%s\n\tCalculation time: \t%s"
-          .format(irr, duration))
-    }
-  }
-
-  def calculateWithActors(nrOfWorkers: Int, maxIterationCount: Int, absoluteAccuracy: Double) {
+  def calculateWithActors(nrOfWorkers: Int, nrOfMessages:Int, maxIterationCount: Int, absoluteAccuracy: Double) {
+	
+    val latch = new CountDownLatch(1)
     // Create an Akka system
     val system = ActorSystem("IrrSystem")
-
+    
+    val buf = ListBuffer.empty[Double]
+    
     // create the result listener, which will print the result
-    val listener = system.actorOf(Props[Listener], name = "listener")
-
+    val listener = system.actorOf(Props(new Listener(buf)), name = "listener")
+    
     // create the master
     val master = system.actorOf(Props(new Master(
-      nrOfWorkers, maxIterationCount, absoluteAccuracy, listener)),
+      nrOfWorkers, nrOfMessages, maxIterationCount, absoluteAccuracy, listener, latch)),
       name = "master")
 
-    // start the calculation
-//    println("\n\tStarting Calculation at: \t%s"
-//      .format(System.currentTimeMillis))
-    1 to 50000 foreach { _ => master ! Calculate(0.05, Array(-1000, 50, 50, 50, 50, 1035)) }
-    1 to 50000 foreach { _ => master ! Calculate(0.05, Array(-1000, 70, 70, 70, 70, 1070)) }
-//    println("\n\tFinished Starting Calculation at: \t%s"
-//      .format(System.currentTimeMillis))
+    1 to 250000 foreach { _ => master ! Calculate(maxIterationCount, absoluteAccuracy, Array(-1000, 50, 50, 50, 50, 1035)) }
+    1 to 250000 foreach { _ => master ! Calculate(maxIterationCount, absoluteAccuracy, Array(-1000, 70, 70, 70, 70, 1070)) }
 
-  }
+    latch.await
+    
+    println("\n\tStopping: \t\t%s"
+            		.format(buf.length))
+    system.shutdown
 
-  def calculate(maxIterationCount: Int, absoluteAccuracy: Double) {
-    val start = System.currentTimeMillis
-    1 to 50000 foreach { _ =>
-      println("\n\tIRR approximation: \t\t%s\n\tCalculation time: \t%s"
-        .format(untilLessThan(maxIterationCount, absoluteAccuracy, Array(-1000, 50, 50, 50, 50, 1035)), System.currentTimeMillis - start))
-    }
-    1 to 50000 foreach { _ =>
-      println("\n\tIRR approximation: \t\t%s\n\tCalculation time: \t%s"
-        .format(untilLessThan(maxIterationCount, absoluteAccuracy, Array(-1000, 70, 70, 70, 70, 1070)), System.currentTimeMillis - start))
-    }
-  }
-
-  def untilLessThan(maxIterationCount: Int, absoluteAccuracy: Double, values: Array[Double]): Double = {
-    var guess = 0.0
-    for (i <- 0 until maxIterationCount) {
-      var result = calculateIrrFor(guess, values)
-      if (Math.abs(result) <= absoluteAccuracy) {
-        return guess - result
-      }
-      guess = guess - result
-    }
-    Double.NaN
   }
 }
